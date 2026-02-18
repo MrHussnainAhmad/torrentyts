90300653c364b894216915fb705a4b15c77433d3
MrHussnainAhmad <mrhussnainahmad@gmail.com>
2026-02-18 08:34:39 +0500

    fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#171717" />
+        <meta name="google-site-verification" content="GxoWjVvXFK0dKlz7eskpWguKgf0w2kF1oSZyaVUjtBg" />
       </Head>
       <body className="antialiased">
         <Main />
